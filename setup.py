fa70faa3555c448598caab7521465945af3f91ec
repole <n.repole@gmail.com>
2020-07-26 04:03:43 -0500

    Remove PyPy support for now thanks to issues with pyodbc, pysqlite, and psycopg2 compat.

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Database :: Front-Ends",
         "Operating System :: OS Independent"
     ]
