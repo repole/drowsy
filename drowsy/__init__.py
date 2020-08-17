4dab6738538d6ab23075fae73c1e3cf23d2d2e03
repole <n.repole@gmail.com>
2020-08-17 01:09:53 -0500

    Fix copyright notice and unnecessary import check in root module.

--- drowsy/__init__.py
+++ drowsy/__init__.py
@@ -4,17 +4,12 @@
 
     Root of the Drowsy module.
 
-    :copyright: (c) 2016-2020 by Nicholas Repole and contributors.
-                See AUTHORS for more details.
-    :license: MIT - See LICENSE for more details.
 """
+# :copyright: (c) 2016-2020 by Nicholas Repole and contributors.
+#             See AUTHORS for more details.
+# :license: MIT - See LICENSE for more details.
 import logging
-try:
-    from logging import NullHandler
-except ImportError:  # pragma: no cover
-    class NullHandler(logging.Handler):
-        def emit(self, record):
-            pass
+from logging import NullHandler
 
 logging.getLogger(__name__).addHandler(NullHandler())
 
